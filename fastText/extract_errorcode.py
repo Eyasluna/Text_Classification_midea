bfcaa7e4a8cb2962ad22635d5767c5fc72fce606
Hawkmoon <yfu13@ucsc.edu>
2018-09-20 11:12:57 -0700

    Create extract_errorcode.py

---
+++ fastText/extract_errorcode.py
@@ -0,0 +1,28 @@
+#-*- coding: UTF-8 -*-
+# coding=<encoding name>
+import re
+import itertools
+
+newmodels = []
+lines = open("/Users/tikacrota/desktop/qa__ac_displaying_errorcode.txt").readlines()
+print ("lines is :",lines)
+
+for keys in lines:
+    p2 = r"\S+?\|"
+    patterns= re.compile(p2)
+    #newkeys = re.search(patterns,keys)
+    newmodels.append(patterns.findall(keys))
+
+structed_keys = list(itertools.chain(*newmodels))
+newTest = set(structed_keys)
+print (newTest)
+
+final_error_set = set()
+for error in structed_keys:
+    code = error.replace('|', '')
+    final_error_set.add(code)
+
+print ("newmodels:", newmodels)
+print ("structed keys:", structed_keys)
+print ("final error code set:", final_error_set)
+print ("numbers of error codes in total:", len(final_error_set))
