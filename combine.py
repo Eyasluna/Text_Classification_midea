03979c4cb883884db2ed7802eb64931103c20c7e
Hawkmoon <yfu13@ucsc.edu>
2019-04-11 16:18:54 -0700

    fixed file process function; combine.py completed

--- combine.py
+++ combine.py
@@ -7,34 +7,49 @@ def file_process(file_name):
     input_file = file_name
     f1 = open(input_file,encoding='utf8').readlines()
 
-    remove_lines= []
-    for keys in f1:
-        keys = keys.strip()
-        p = r'\-.'
-        pattern = re.compile(p)
-        remove_lines.append(pattern.findall(keys))
-    remove_lines = set(list(itertools.chain(*remove_lines)))
+    # remove_lines= []
+    # for keys in f1:
+    #     keys = keys.strip()
+    #     p = r'..-.'
+    #     pattern = re.compile(p)
+    #     remove_lines.append(pattern.findall(keys))
+    # remove_lines = set(list(itertools.chain(*remove_lines)))
 
-    with open(file_name, 'w+',encoding='utf-8') as w1:
-        for line in f1:
-            for keys in remove_lines:
-                line = line.replace(keys,'')
-                line = line.strip()
-                w1.write(line+'\n')
+    f2 = open(file_name, 'w+', encoding='utf-8')
+    for line in f1:
+        newline = line[4:]
+        f2.write(newline )
+    f2.close()
+
+    # f3 = open(file_name, 'w+', encoding='utf-8')
+    # for line in f1:
+    #     for keys in remove_lines:
+    #         line = line.replace(keys,'')
+    #     f3.write(line)
+    # f3.close()
 
 def add_name(file_path):
+    names = os.listdir(file_path)
+    new_list = []
+    for name in names:
+        new_list.append(file_path+name)
+    return new_list
+
+def combine_files(file_path):
     file_names = os.listdir(file_path)
-    newlist = []
-    for name in file_names:
-        newlist.append(file_path+name)
-    return newlist
-
-
-print(add_name('/Users/tikacrota/Desktop/Midea/Text_Classification_midea/english/'))
-for files in add_name('/Users/tikacrota/Desktop/Midea/Text_Classification_midea/english/'):
-    file_process(files)
-    print("Done")
+    print(file_names)
+    f = open('result.txt','w+', encoding='utf-8')
+    for files in add_name(file_path):
+        for lines in open(files):
+            f.writelines(lines)
+    f.close()
 
 
 
+print('name:', add_name('/Users/tikacrota/Desktop/Midea/Text_Classification_midea/english/'))
+# for files in add_name('/Users/tikacrota/Desktop/Midea/Text_Classification_midea/english/'):
+#     file_process(files)
+#     print("Done")
+#
+combine_files('/Users/tikacrota/Desktop/Midea/Text_Classification_midea/english/')
 
